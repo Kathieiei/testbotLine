52c51a28e934b5bc3ab47f9283902bca9ff46cab
Kathieiei <hengjamrat_t@silpakorn.edu>
2021-11-22 04:44:28 +0700

    testpush6

--- test4.php
+++ test4.php
@@ -1,37 +1,31 @@
 <?php
-   $accessToken = "Xz5YorliDxlUasYbtcV1PG8hNQh6Ce4KGTWNUkwAdCEsSzfRaa/g2CdtQuGrmkSSwxAFKRni8LxMoM+8vufU1aT0tJswTReq3FfVi8pp+5NduiLAhZUHuS2/yF1A6ZEZ6KJKRxdFeFvad6XmTfwYHgdB04t89/1O/w1cDnyilFU=";//copy ข้อความ Channel access token ตอนที่ตั้งค่า
-   $content = file_get_contents('php://input');
-   $arrayJson = json_decode($content, true);
-   $arrayHeader = array();
-   $arrayHeader[] = "Content-Type: application/json";
-   $arrayHeader[] = "Authorization: Bearer {$accessToken}";
-   //รับข้อความจากผู้ใช้
-   $message = $arrayJson['events'][0]['message']['text'];
-   //รับ id ของผู้ใช้
-   $id = 'Ud34e97f0f96077eadfe7f2d339f87266';
+// Access Token
+$access_token = 'Xz5YorliDxlUasYbtcV1PG8hNQh6Ce4KGTWNUkwAdCEsSzfRaa/g2CdtQuGrmkSSwxAFKRni8LxMoM+8vufU1aT0tJswTReq3FfVi8pp+5NduiLAhZUHuS2/yF1A6ZEZ6KJKRxdFeFvad6XmTfwYHgdB04t89/1O/w1cDnyilFU=';
+// User ID
+$userId = 'Ud34e97f0f96077eadfe7f2d339f87266';
+// ข้อความที่ต้องการส่ง
+$messages = array(
+    'type' => 'text',
+    'text' => 'ทดสอบการส่งข้อความ',
+);
+$post = json_encode(array(
+    'to' => array($userId),
+    'messages' => array($messages),
+));
+// URL ของบริการ Replies สำหรับการตอบกลับด้วยข้อความอัตโนมัติ
+$url = 'https://api.line.me/v2/bot/message/multicast';
+$headers = array('Content-Type: application/json', 'Authorization: Bearer '.$access_token);
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+echo $result;
 
      
-          $arrayPostData['to'] = 'Ud34e97f0f96077eadfe7f2d339f87266';
-          $arrPostData['messages'][0]['type'] = "text";
-          $arrPostData['messages'][0]['text'] = "สวัสดี เราคือแชทบอทที่ช่วยในการดูแลงานระหว่างเจ้าของรถที่มีรถหลายๆคน กับเหล่าคนขับรถที่เจ้าของรถจ้างมา และดูแลในส่วนของการบันทึกการทำงานระหว่างผู้จ้างที่มาจ้างคนเจ้าของรถอีกด้วย";
-          $arrPostData['messages'][1]['type'] = "sticker";
-          $arrPostData['messages'][1]['packageId'] = "11537";
-          $arrPostData['messages'][1]['stickerId'] = "52002739";
-          pushMsg($arrayHeader,$arrayPostData);
     
  
-   function pushMsg($arrayHeader,$arrayPostData){
-      $strUrl = "https://api.line.me/v2/bot/message/push";
-      $ch = curl_init();
-      curl_setopt($ch, CURLOPT_URL,$strUrl);
-      curl_setopt($ch, CURLOPT_HEADER, false);
-      curl_setopt($ch, CURLOPT_POST, true);
-      curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
-      curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayPostData));
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
-      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-      $result = curl_exec($ch);
-      curl_close ($ch);
-   }
-   exit;
+
 ?>

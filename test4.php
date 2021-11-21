28fc3978003cd41b4b1f5d210f1ff1ba7186911d
Kathieiei <hengjamrat_t@silpakorn.edu>
2021-11-22 04:12:25 +0700

    testpush1

--- test4.php
+++ test4.php
@@ -1,40 +1,34 @@
 <?php
- 
-$strAccessToken = "Xz5YorliDxlUasYbtcV1PG8hNQh6Ce4KGTWNUkwAdCEsSzfRaa/g2CdtQuGrmkSSwxAFKRni8LxMoM+8vufU1aT0tJswTReq3FfVi8pp+5NduiLAhZUHuS2/yF1A6ZEZ6KJKRxdFeFvad6XmTfwYHgdB04t89/1O/w1cDnyilFU=";
- 
-$content = file_get_contents('php://input');
-$arrJson = json_decode($content, true);
- 
-$strUrl = "https://api.line.me/v2/bot/message/reply";
-$strUrl = "https://api.line.me/v2/bot/message/push";
- 
-$arrHeader = array();
-$arrHeader[] = "Content-Type: application/json";
-$arrHeader[] = "Authorization: Bearer {$strAccessToken}";
- 
-// if($arrJson['events'][0]['message']['text'] == "ไอดี"){
-//   $arrPostData = array();
-//   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-//   $arrPostData['messages'][0]['type'] = "text";
-//   $arrPostData['messages'][0]['text'] = "ID คุณคือ ".$arrJson['events'][0]['source']['userId'];
-// }
-    $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('<Xz5YorliDxlUasYbtcV1PG8hNQh6Ce4KGTWNUkwAdCEsSzfRaa/g2CdtQuGrmkSSwxAFKRni8LxMoM+8vufU1aT0tJswTReq3FfVi8pp+5NduiLAhZUHuS2/yF1A6ZEZ6KJKRxdFeFvad6XmTfwYHgdB04t89/1O/w1cDnyilFU=>');
-    $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '<8e4788f8f2b0a590fe809041521f1bf6>']);
-
-    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-    $response = $bot->pushMessage('<Ud34e97f0f96077eadfe7f2d339f87266>', $textMessageBuilder);
-
-    echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
- 
-// $ch = curl_init();
-// curl_setopt($ch, CURLOPT_URL,$strUrl);
-// curl_setopt($ch, CURLOPT_HEADER, false);
-// curl_setopt($ch, CURLOPT_POST, true);
-// curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
-// curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
-// curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
-// curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-// $result = curl_exec($ch);
-// curl_close ($ch);
- 
+   $accessToken = "";//copy ข้อความ Channel access token ตอนที่ตั้งค่า
+   $content = file_get_contents('php://input');
+   $arrayJson = json_decode($content, true);
+   $arrayHeader = array();
+   $arrayHeader[] = "Content-Type: application/json";
+   $arrayHeader[] = "Authorization: Bearer {$accessToken}";
+   //รับข้อความจากผู้ใช้
+   $message = $arrayJson['events'][0]['message']['text'];
+   //รับ id ของผู้ใช้
+   $id = 'Ud34e97f0f96077eadfe7f2d339f87266';
+   #ตัวอย่าง Message Type "Text + Sticker"
+      $arrayPostData['to'] = $id;
+      $arrayPostData['messages'][0]['type'] = "text";
+      $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา";
+      $arrayPostData['messages'][1]['type'] = "sticker";
+      $arrayPostData['messages'][1]['packageId'] = "2";
+      $arrayPostData['messages'][1]['stickerId'] = "34";
+      pushMsg($arrayHeader,$arrayPostData);
+   function pushMsg($arrayHeader,$arrayPostData){
+      $strUrl = "https://api.line.me/v2/bot/message/push";
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_URL,$strUrl);
+      curl_setopt($ch, CURLOPT_HEADER, false);
+      curl_setopt($ch, CURLOPT_POST, true);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrayPostData));
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+      $result = curl_exec($ch);
+      curl_close ($ch);
+   }
+   exit;
 ?>
